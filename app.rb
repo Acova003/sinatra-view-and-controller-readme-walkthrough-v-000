50a3ef181b9ca51526fdae6adec82b698a3efaef
Amee Covarrubias <ameecova@yahoo.com>
2019-06-08 01:09:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,9 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
-    puts params
+    original_string = params["string"]
+    reversed_string = original_string.reverseputs params
+    
     erb :reversed
   end
 
